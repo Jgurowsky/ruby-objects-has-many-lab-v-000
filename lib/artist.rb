d08af47e0c179212e7b8a2e19f9e4e76ddd1cee0
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-08-08 03:05:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,12 +13,15 @@ class Artist
   end
 
   def add_song(song)
-    # binding.pry
-    # line "19" connects the two classes
-    # "song" refers to instance of the "Song" class. artist refers to this songs artist attribute
-    # in that Song class, and we are setting it euqal to the Instance of the
-    # Artist class
+    # line 20 connects the two classes
+    # song refers to an instance of the Song class
+    # artist refers to this songs artist attribute
+    # artist attribute is set to the instance of the Artist class
     song.artist = self
     @songs << song
+  end
+
+  def add_song_by_name
+
   end
 end
